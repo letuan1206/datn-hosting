7a87811149145072553c86c7bec922df4d2db750
Le Anh Tuan <tuanleanh1994@gmail.com>
2017-11-13 15:00:52 +0700

    card phone

--- apps/card/card_phone.js
+++ apps/card/card_phone.js
@@ -4,11 +4,20 @@
         .controller('CardPhoneCtrl', CardPhoneCtrl);
 
     function CardPhoneCtrl($scope, $rootScope, $http, $window, toastr) {
+        
+        var account = JSON.parse($window.sessionStorage.getItem(LOCALSTORAGE_USER)).memb___id;
 
-        $scope.dtOptions = DTOptionsBuilder.newOptions()
-            .withPaginationType('full_numbers')
-            .withOption('lengthChange', false)
-            .withOption('searching', false)
-            .withDisplayLength(50);
+        var url = SERVER_API + 'bank/getBankInfo?account=' + account;
+
+        $http.get(url, set_header(), {
+            withCredentials: true
+        }).then(function (response) {
+            $scope.res = response.data;
+            $rootScope.bankInfo = response.data.data;
+            $window.sessionStorage.setItem(LOCALSTORAGE_BANKINFO, JSON.stringify(response.data.data));
+        }, function (err) {
+            $scope.isServerError = false;
+        });
+       
     }
 })();
