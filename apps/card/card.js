41661c3c1795c2bc62f1512a36814428c8db0014
tuanlepg <tuan.le@powergatesoftware.com>
2017-11-13 17:47:20 +0700

    card charge

--- apps/card/card.js
+++ apps/card/card.js
@@ -1,9 +1,21 @@
-(function() {
+(function () {
     angular
         .module('app')
         .controller('CardCtrl', CardCtrl);
 
     function CardCtrl($scope, $rootScope, $http, $window, toastr) {
-        
+        $scope.showCard = function () {
+            var account = JSON.parse($window.sessionStorage.getItem(LOCALSTORAGE_USER)).memb___id;
+            var url = SERVER_API + 'card/getCardHistory?account=' + account;
+
+            $http.get(url, set_header(), {
+                withCredentials: true
+            }).then(function (response) {
+                $scope.listCard = response.data.data;
+                $scope.show = true;
+            }, function (err) {
+                $scope.isServerError = false;
+            });
+        }
     }
 })();
--- apps/card/card_phone.js
+++ apps/card/card_phone.js
@@ -4,20 +4,86 @@
         .controller('CardPhoneCtrl', CardPhoneCtrl);
 
     function CardPhoneCtrl($scope, $rootScope, $http, $window, toastr) {
-        
-        var account = JSON.parse($window.sessionStorage.getItem(LOCALSTORAGE_USER)).memb___id;
+        $scope.listCard = [
+            {
+                "type": "Viettel",
+                "k10000": 100,
+                "k20000": 200,
+                "k30000": 300,
+                "k50000": 520,
+                "k100000": 1040,
+                "k200000": 2080,
+                "k300000": 3120,
+                "k500000": 5200,
+                "k1000000": 10400
+            },
+            {
+                "type": "Mobiphone",
+                "k10000": 100,
+                "k20000": 200,
+                "k30000": 300,
+                "k50000": 520,
+                "k100000": 1040,
+                "k200000": 2080,
+                "k300000": 3120,
+                "k500000": 5200,
+                "k1000000": 10400
+            },
+            {
+                "type": "Vinaphone",
+                "k10000": 100,
+                "k20000": 200,
+                "k30000": 300,
+                "k50000": 520,
+                "k100000": 1040,
+                "k200000": 2080,
+                "k300000": 3120,
+                "k500000": 5200,
+                "k1000000": 10400
+            },
+            {
+                "type": "GATE",
+                "k10000": 100,
+                "k20000": 200,
+                "k30000": 300,
+                "k50000": 550,
+                "k100000": 1100,
+                "k200000": 2200,
+                "k300000": 3300,
+                "k500000": 5500,
+                "k1000000": 11000
+            }
+        ]
 
-        var url = SERVER_API + 'bank/getBankInfo?account=' + account;
+        $scope.submit = function () {
+            var account = JSON.parse($window.sessionStorage.getItem(LOCALSTORAGE_USER)).memb___id;
+            var url = SERVER_API + 'card/chargeCard';
 
-        $http.get(url, set_header(), {
-            withCredentials: true
-        }).then(function (response) {
-            $scope.res = response.data;
-            $rootScope.bankInfo = response.data.data;
-            $window.sessionStorage.setItem(LOCALSTORAGE_BANKINFO, JSON.stringify(response.data.data));
-        }, function (err) {
-            $scope.isServerError = false;
-        });
-       
+            var data = {
+                account: account,
+                cardType: $scope.cardType,
+                cardCode: $scope.cardCode,
+                cardSeri: $scope.cardSeri,
+            };
+
+            $http.post(url, data, set_header(), {
+                withCredentials: true
+            }).then(function (response) {
+                $scope.res = response.data;
+                if (response.data.status === RESPONSE_STATUS_SUCCESS) {
+                    $scope.cardCode = '';
+                    $scope.cardSeri = '';
+                    toastr.success(response.data.message, {
+                        closeButton: true
+                    });
+                } else if (response.data.status === RESPONSE_STATUS_ERROR) {
+                    toastr.error(response.data.message, {
+                        closeButton: true
+                    });
+                }
+            }, function (err) {
+                $scope.isServerError = false;
+            });
+        }
     }
 })();
